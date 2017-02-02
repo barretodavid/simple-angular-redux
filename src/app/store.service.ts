e179a58b98a1d919b515f7ae1a3a58d72b39a555
David Barreto <barretollano@gmail.com>
2017-02-02 15:48:36 -0500

    Initial commit

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -4,15 +4,19 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { Todo } from './todo.model';
 import { todoReducer, INIT, Action } from './todo.reducer';
 
+const rootReducer = {
+  todos: todoReducer
+}
+
 @Injectable()
 export class StoreService {
-  state$: Observable<any>;
-  action$: BehaviorSubject<any>;
+  state$: Observable<Todo[]>;
+  action$: BehaviorSubject<Action>;
 
   constructor() {
-    this.action$ = new BehaviorSubject<any>({ type: INIT });
+    this.action$ = new BehaviorSubject<Action>({ type: INIT });
     this.state$ = this.action$
-      .scan((todos, action) => todoReducer(todos, action), []);
+      .scan<Action, Todo[]>((todos, action) => todoReducer(todos, action), []);
   }
 
   select(): Observable<Todo[]> {
